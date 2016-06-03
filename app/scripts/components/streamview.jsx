c77707c2844244fd06d4b03c0026c3971b5aa1a3
schmedes <julian.schmedes@gmx.de>
2016-06-03 15:33:51 +0200

    add border around streams

--- app/scripts/components/streamview.jsx
+++ app/scripts/components/streamview.jsx
@@ -26,18 +26,8 @@ export default class StreamView extends Component {
    return (
        <div className={this.props.streamname + ' streamcontainer'}>
        <div className="buttoncontainer">
-       <button onClick={()=> this.deleteFrame(this.props.streamindex)}>
-       <svg fill="#FFFFFF" height="20" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg">
-           <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
-           <path d="M0 0h24v24H0z" fill="none"/>
-       </svg>
-       </button>
-       <button onClick={()=> this.openChat(this.props.streamname)}>
-       <svg fill="#FFFFFF" height="20" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg">
-           <path d="M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zM6 9h12v2H6V9zm8 5H6v-2h8v2zm4-6H6V6h12v2z"/>
-           <path d="M0 0h24v24H0z" fill="none"/>
-       </svg>
-       </button>
+       <button onClick={()=> this.deleteFrame(this.props.streamindex)}>Remove</button>
+       <button onClick={()=> this.openChat(this.props.streamname)}>Chat</button>
        <button onClick={()=> this.toggleFullScreen(this.props.streamname)}>Fill</button>
        </div>
        <div className= "streamview">
